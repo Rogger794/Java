846c749610ce4f1962fcdb5c2edeb1c1755ff7c6
Rogger794 <rogger_jrj794@hotmail.com>
2015-09-02 22:09:53 -0500

    Programa que muestra el uso de JComboBox

---
+++ Java/CuadroCombinado/MarcoCuadroCombinado.java
@@ -0,0 +1,48 @@
+package CuadroCombinado;
+
+import java.awt.FlowLayout;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class MarcoCuadroCombinado extends JFrame
+{
+	private JComboBox imagenesJ;
+	private JLabel etiqueta;
+	
+	private String nombres[]={"mario.png", "hongo.png", "koopa.png", "enemigo.png"};
+	
+	private Icon iconos[]={
+		new ImageIcon(getClass().getResource(nombres[0])),
+		new ImageIcon(getClass().getResource(nombres[1])),
+		new ImageIcon(getClass().getResource(nombres[2])),
+		new ImageIcon(getClass().getResource(nombres[3]))};
+	
+	public MarcoCuadroCombinado()
+	{
+		super("Prueba de JComboBox");
+		setLayout(new FlowLayout());
+		
+		imagenesJ=new JComboBox(nombres);
+		imagenesJ.setMaximumRowCount(3);
+		
+		imagenesJ.addItemListener(
+			new ItemListener()
+			{
+				public void itemStateChanged(ItemEvent evento)
+				{
+					if(evento.getStateChange()==ItemEvent.SELECTED)
+						etiqueta.setIcon(iconos[imagenesJ.getSelectedIndex()]);
+				}
+			}
+		);
+		
+		add(imagenesJ);
+		etiqueta=new JLabel(iconos[0]);
+		add(etiqueta);
+	}
+}
---
+++ Java/CuadroCombinado/PruebaCuadroCombinado.java
@@ -0,0 +1,14 @@
+package CuadroCombinado;
+
+import javax.swing.JFrame;
+
+public class PruebaCuadroCombinado
+{
+	public static void main(String args[])
+	{
+		MarcoCuadroCombinado marco=new MarcoCuadroCombinado();
+		marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		marco.setSize(350,150);
+		marco.setVisible(true);
+	}
+}
